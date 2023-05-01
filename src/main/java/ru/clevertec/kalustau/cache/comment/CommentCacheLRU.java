2f5e7bdea8e60d743deeba0b15c50fc6a29b5b9b
jskov259259 <jskov259259@rambler.ru>
2023-05-01 15:46:48 +0300

    fix: fix bag in CommentCacheLRU

--- src/main/java/ru/clevertec/kalustau/cache/comment/CommentCacheLRU.java
+++ src/main/java/ru/clevertec/kalustau/cache/comment/CommentCacheLRU.java
@@ -1,9 +1,9 @@
 package ru.clevertec.kalustau.cache.comment;
 
-import ru.clevertec.kalustau.cache.CacheLFU;
+import ru.clevertec.kalustau.cache.CacheLRU;
 import ru.clevertec.kalustau.model.Comment;
 
-public class CommentCacheLRU extends CacheLFU<Comment> {
+public class CommentCacheLRU extends CacheLRU<Comment> {
 
     /**
      * Constructor - new Comment Cache LFU object
